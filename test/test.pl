1f9a57e5adb1b111c0254c6065028442ea4dbb55
Rajesh-sajjan <rajesh.sajjan@xad.com>
2016-03-08 14:35:42 -0800

    teamcity

--- test/test.pl
+++ test/test.pl
@@ -3,6 +3,6 @@ $datestring = localtime();
 open(my $fh, '>>', '../outpit.txt') or die;
 print $fh "Running from teamcity goggles $datestring";
 close $fh;
-##teamcity[testSuiteStarted name='suiteName']
+teamcity[testSuiteStarted name='suiteName']
 <individual test messages go here>
-##teamcity[testSuiteFinished name='suiteName']
+teamcity[testSuiteFinished name='suiteName']
