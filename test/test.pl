c2ba924b14d9df13ab3217cc26619286d62d5dbb
Rajesh-sajjan <rajesh.sajjan@xad.com>
2016-03-08 15:38:46 -0800

    TC

--- test/test.pl
+++ test/test.pl
@@ -4,5 +4,6 @@ open(my $fh, '>>', '../outpit.txt') or die;
 print $fh "Running from teamcity goggles $datestring";
 close $fh;
 print "##teamcity[testSuiteStarted name='suiteName']";
-<individual test messages go here>;
+print "##teamcity[testStarted name='mytest']";
+print "##teamcity[testFinished name='mytest']";
 print "##teamcity[testSuiteFinished name='suiteName']";
