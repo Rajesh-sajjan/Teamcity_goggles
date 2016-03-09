eeb52544ba3d1ed3ba21f8b8a5ac6ecffa6b1885
Rajesh-sajjan <rajesh.sajjan@xad.com>
2016-03-09 11:17:35 -0800

    TC

--- test/test.pl
+++ test/test.pl
@@ -5,6 +5,6 @@ print $fh "Running from teamcity goggles $datestring";
 close $fh;
 print "##teamcity[testSuiteStarted name='suiteName']";
 print "##teamcity[testStarted name='mytest']";
-print "##teamcity[testFailed name='mtest' message='failure message' details='message and stack trace']";
+print "##teamcity[testFailed name='mtest' message='\"$datestring\"' details='message and stack trace']";
 print "##teamcity[testFinished name='mytest']";
 print "##teamcity[testSuiteFinished name='suiteName']";
